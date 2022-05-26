924ce7199c35d1e27ab671503e22ad2de41f684e
Werfmon <d.vyroubal.w@gmail.com>
2022-05-26 09:09:35 +0200

    login fix

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,11 @@ import '../styles/globals.css'
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 function MyApp({ Component, pageProps }) {
+
+  window.onload(() => {
+    document.title = "ProeniX"
+  })
+
   return <Component {...pageProps} />
 }
 
